10c06c77fe5f70982aa3a7b72f5d9c4bf324b820
lchristofoletti <99030871+lchristofoletti@users.noreply.github.com>
2022-03-17 16:43:59 -0300

    Att Estudos String

--- Estudos_String.py
+++ Estudos_String.py
@@ -6,7 +6,7 @@ cpf = cpf.replace(".", "")
 cpf = cpf.replace("-", "")
 # comando len utilizado para sinalizar que está se referindo a quantidade de caracter
 # comando isnumeric para identificação que o caracter é numero
-if len(cpf) == 11 and cpf.isnumeric:
+if len(cpf) == 11 and cpf.isnumeric():
     print("Seu CPF é : {}".format(cpf))
 else:
     print("Favor inserir os 11 numeros do seu CPF (SOMENTE NUMEROS)")
